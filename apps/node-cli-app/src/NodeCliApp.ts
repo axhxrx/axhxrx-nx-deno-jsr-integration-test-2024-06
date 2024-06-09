945cf4d7584d62d3099a01bc4c8a571d4dc6670b
protiev <protiev@axhxrx.com>
2024-06-09 06:17:36 +0000

    feat: make NodeCliApp self-aware

---
+++ apps/node-cli-app/src/NodeCliApp.ts
@@ -0,0 +1,11 @@
+export class NodeCliApp {
+  constructor(
+    readonly id: string = `NodeCliApp created at ${new Date().toISOString()}`
+  ) {
+    // 🌹 Hi, mom!
+  }
+
+  main(): void {
+    console.log(`Hello, this is ${this.id}`);
+  }
+}
--- apps/node-cli-app/src/main.ts
+++ apps/node-cli-app/src/main.ts
@@ -1,1 +1,4 @@
-console.log('Hello World');
+import { NodeCliApp } from "./NodeCliApp";
+
+const app = new NodeCliApp();
+app.main();
